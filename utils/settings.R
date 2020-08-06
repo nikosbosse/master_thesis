ac9f06516758efd52ca4539880a83689214d2009
nikosbosse <nikosbosse@gmail.com>
2020-08-06 19:07:22 +0200

    updated model names for evaluation in settings

--- utils/settings.R
+++ utils/settings.R
@@ -45,6 +45,10 @@ model_names <- c("COVIDhub-baseline",
                  "UMass-MechBayes",
                  "YYG-ParamSearch")
 
+
+model_names_eval <- c(model_names,
+                      "crps-ensemble", "mean-ensemble", "qra-ensemble")
+
 submission_dates <- lubridate::ymd("2020-07-27") - seq(0, 13 * 7, 7)
 submission_dates <- submission_dates[1:7]
 
@@ -64,6 +68,7 @@ settings <- list(submission_dates = submission_dates,
                  forecast_date = forecast_date,
                  target_end_dates = target_end_dates,
                  model_names = model_names,
+                 model_names_eval = model_names_eval,
                  n_samples = n_samples,
                  num_last = num_last,
                  horizons = horizons)
